68dc688742cc222dc7380880d5ad143545e1a88d
Amal Anan <133678416+amalanan@users.noreply.github.com>
2025-07-11 20:54:35 +0300

    Create todo_list.dart

---
+++ lib/utils/todo_list.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+    required this.deleteFunction,
+  });
+
+  final String taskName;
+  final bool taskCompleted;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, top: 15, bottom: 0),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
+          children: [
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(15),
+              backgroundColor: Colors.deepPurple.shade200,
+            ),
+          ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            color: Colors.deepPurple,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                checkColor: Colors.black,
+                activeColor: Colors.white,
+                side: BorderSide(color: Colors.white),
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  decoration:
+                      taskCompleted
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                  decorationColor: Colors.white,
+                  decorationThickness: 2,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
